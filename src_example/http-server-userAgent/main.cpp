aaa50f5ebf50cb720a981dd7e0cc6b90d06ba21d
bradosia <bradosia@gmail.com>
2020-11-06 20:32:14 -0800

    uap-cpp

---
+++ src_example/http-server-userAgent/main.cpp
@@ -0,0 +1,85 @@
+/*
+ * @name BookFiler Module - HTTP w/ Curl
+ * @author Branden Lee
+ * @version 1.00
+ * @license MIT
+ * @brief HTTP module for BookFiler™ applications.
+ */
+
+// Bookfiler Modules
+#include <BookFilerModuleHttpLoader.hpp>
+
+std::string routeAbout(bookfiler::HTTP::request req,
+                       bookfiler::HTTP::response res);
+int allModulesLoaded();
+void signal_handler(int signal);
+
+std::string testName = "HTTP Server Example";
+std::shared_ptr<bookfiler::HTTP::ModuleInterface> httpModule;
+std::shared_ptr<bookfiler::HTTP::Server> httpServer;
+
+int main() {
+  std::cout << testName << " BEGIN" << std::endl;
+
+  bookfiler::HTTP::loadModule("modules", std::bind(&allModulesLoaded),
+                              httpModule);
+
+  std::cout << testName << " END" << std::endl;
+  return 0;
+}
+
+int allModulesLoaded() {
+  // SSL Certificate manager
+  std::shared_ptr<bookfiler::certificate::Manager> certificateManager =
+      httpModule->newCertificateManager();
+  std::shared_ptr<bookfiler::certificate::Certificate> certRootPtr,
+      certServerPtr;
+  certificateManager->loadCertificate(certRootPtr);
+
+  // Setup server
+  httpServer = httpModule->newServer({{"port", 3000}, {"host", "localhost"}});
+  httpServer->useCertificate(certRootPtr);
+
+  httpServer->route({{"method", "GET"},
+                     {"path", "/get"},
+                     {"handler",
+                      [](bookfiler::HTTP::request req,
+                         bookfiler::HTTP::response res) -> std::string {
+                        // Get cookie
+                        std::string_view value =
+                            req->getCookie("name")->value();
+                        std::string bodyStr;
+                        bodyStr.append("Get cookie name=").append(value);
+                        return bodyStr;
+                      }}});
+
+  httpServer->route({{"method", "GET"},
+                     {"path", "/user"},
+                     {"handler",
+                      [](bookfiler::HTTP::request req,
+                         bookfiler::HTTP::response res) -> std::string {
+                        // Get cookie
+                        std::string value = req->getUserAgentBrowser();
+                        std::string bodyStr;
+                        bodyStr.append("User Agent Browser=").append(value);
+                        return bodyStr;
+                      }}});
+
+  httpServer->route(
+      {{"method", "GET"},
+       {"path", "*"},
+       {"handler",
+        [](bookfiler::HTTP::request req,
+           bookfiler::HTTP::response res) -> std::string {
+          // Set cookie
+          res->setCookie("name", "value", {});
+          return "<h1>Set cookie</h1><br><a href=\"/get\">Get Cookie</a>";
+        }}});
+
+  // Start server
+  httpServer->run();
+
+  httpModule->wait("exit");
+
+  return 0;
+}
---
+++ third_party/uap-cpp/uap-cpp/README.md
@@ -0,0 +1,48 @@
+ua_parser C++ Library
+=====================
+
+Usage
+-----
+
+### Linux
+
+To build the (static) library:
+
+    make uaparser_cpp
+
+To build and run the tests:
+
+    make test
+
+A recent (GCC >= 4.8 or Clang >= 3.9 both work) C++11 compiler is required.
+
+### Windows
+
+First, open ``uap-cpp.sln`` with MSVC 15 (Visual Studio 2017).
+
+To build the (static) library:
+
+    build the "UaParser" project
+
+To build and run the tests:
+
+    build the "UaParserTest" project
+
+The MSVC projects assume boost to be installed at: ``C:\boost_1_69_0`` and yaml to be installed at: ``C:\yaml-cpp``. Change these paths if needed. ``boost_regex`` needs to be built from source in advance. For yaml-cpp, you can built it from source, or use a prebuilt [here](https://github.com/hsluoyz/yaml-cpp-prebuilt-win32).
+
+Dependencies
+------------
+
+* re2, yaml-cpp (0.5 API)
+* gtest (for testing)
+* [uap-core](https://github.com/ua-parser/uap-core), same directory level as uap-cpp. You can clone this repo with --recurse-submodules to get it. Alternatively, run `git submodule update --init`.
+
+Contributing
+------------
+
+Pull requests are welcome. Use `clang-format -i *.cpp *.h` to format the sources before sending the patch.
+
+Credits
+-------
+
+Inspired from the D implementation by Shripad K and using agent data from BrowserScope.
